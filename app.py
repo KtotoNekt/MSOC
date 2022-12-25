c5309925dff2b016320339a062cbe1d5dd27de39
Ktoto <nektotoaktoto@protonmail.com>
2022-12-25 20:04:26 +0300

    Предупреждение на странице

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def musics():
     with open("musics.json", "r") as fr:
         musics = json.loads(fr.read())
-    source = ""
+    source = f"<p>Если на странице не загружаются некоторые песни, попробуйте перезагрузить страницу и отключить расширения для блокировки рекламы и т. п.</p>"
     for k, v in musics.items():
         source += f'<p>{k}</p><video controls="" src="{v}" autoplay="" style="height: 40px; width: 66%;"></video>'
     return render_template_string(source)
