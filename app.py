eeaa9b52bffb1e36a862fddf757f29cc3f0e045f
Ktoto <nektotoaktoto@protonmail.com>
2023-02-16 16:05:02 +0300

    Ускорен поиск

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def musics():
         musics = json.loads(fr.read())
     source = f"<p>Если на странице не загружаются некоторые песни, попробуйте перезагрузить страницу и отключить расширения для блокировки рекламы и т. п.</p>"
     for k, v in musics.items():
-        source += f'<p>{k}</p><video controls="" src="{v}" autoplay="" style="height: 40px; width: 66%;"></video>'
+        source += f'<p>{k}</p><audio controls="" src="{v}" style="height: 40px; width: 66%;"></audio>'
     return render_template_string(source)
--- msoc.py
+++ msoc.py
@@ -41,22 +41,16 @@ def add_widget_url(track):
     _u = track.find("a", {"class": "track-dl"})["onclick"].split("url=")[1].split("'")[0]
 
     try:
-        with requests.get(_u, stream=True, timeout=10) as data:
-            for content in data.iter_content(2048):
-                content = str(content)
-                if content.startswith("b'<!DOCTYPE html>") or content.startswith("b'<html>"):
-                    doc_music = BeautifulSoup(content, "html.parser")
-                    try:
-                        _u = doc_music.find("meta", {"http-equiv": "refresh"})["content"].split("?url=")[-1]
-                    except:
-                        break
-                    
-                music_urls[name] = _u
-                print("\n" + name + ": " + _u + "\n")
-
-                break
+        headers = requests.head(_u).headers
+        location_file = headers.get("location")
+        
+        if location_file:
+            _u = location_file
+            
+        music_urls[name] = _u
+        print("\n" + name + ": " + _u + "\n")
     except Exception as ex:
-        pass
+        print(ex)
     
     del threads[-1]
 
