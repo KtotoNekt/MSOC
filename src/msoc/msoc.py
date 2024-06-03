a1a1bf7e5556efe52b8bbefba441d21e290a4f3c
Ktoto <136707036+KtotoNekt@users.noreply.github.com>
2024-06-03 19:16:22 +0300

    Функция create_generator_task вынесена в отдельный файл

--- src/msoc/msoc.py
+++ src/msoc/msoc.py
@@ -1,9 +1,8 @@
-import asyncio
 from importlib import util
 from os.path import dirname, join
 from types import ModuleType
-from typing import AsyncGenerator, Any
 
+from .functions import create_generator_task
 from .exceptions import *
 from .sound import Sound
 
@@ -65,32 +64,6 @@ def unload_search_engine(name: str) -> None:
         raise LoadedEngineNotFoundError(name)
 
 
-def __load_default_engines():
-    for name, python_file_path in DEFAULT_ENGINES_MAP.items():
-        load_search_engine(name, python_file_path)
-
-
-async def consume(a_iter):
-    try:
-        return await a_iter.__anext__(),  asyncio.create_task(consume(a_iter))
-    except StopAsyncIteration:
-        return None, None
-
-
-def create_generator_task(gen: AsyncGenerator) -> AsyncGenerator:
-    result_queue = asyncio.create_task(consume(gen.__aiter__()))
-    async def consumer():
-        nonlocal result_queue
-        while 1:
-            item, result_queue = await result_queue
-            if result_queue is None:
-                assert item is None
-                return
-            yield item
-    return consumer()
-
-
-
 async def search(query: str):
     """
     Функция начинает поиск песен по запросу query.
@@ -108,4 +81,7 @@ async def search(query: str):
             yield sound
     
 
-__load_default_engines()
+
+# Загружаем модули по умолчанию
+for name, python_file_path in DEFAULT_ENGINES_MAP.items():
+    load_search_engine(name, python_file_path)
